87070fb4e1095263ff5240807eeb90d3e421111f
Eric Alura <eric.oliveira@alura.com.br>
2022-08-08 16:56:15 -0300

    refatorando para T2

--- src/Conversor/ConversorMoeda.java
+++ src/Conversor/ConversorMoeda.java
@@ -1,53 +1,52 @@
 package Conversor;
 
-import javax.swing.*; 
+import javax.swing.*;
 
 public class ConversorMoeda {
-	 public static void main (String[] args) {
-	
-	Function f = new Function();
-    
+    public static void main(String[] args) {
 
-    while(true) {
-    	
-    	String opcao = (JOptionPane.showInputDialog(null, "Escolha uma opção ", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[] {"Conversor de Moeda", "Conversor de Temperatura"}, "Escolha")).toString();
+        Funcao funcao = new Funcao();
 
-    	if (opcao == "Conversor de Moeda") {
-        String input = JOptionPane.showInputDialog("Insira um valor: ");
-        if(f.check(input) == true) {
-            double Minput = Double.parseDouble(input);
-            f.Converter(Minput);
 
-            int resposta = 0;
-            resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
-            if((resposta == 0) && (f.check(input) == true)) { 
-            } else {
-                JOptionPane.showMessageDialog(null, "Programa finalizado");
-                break;
+        while (true) {
+
+            String opcao = JOptionPane.showInputDialog(null, "Escolha uma opção ", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moeda", "Conversor de Temperatura"}, "Escolha").toString();
+
+            if (opcao.equals("Conversor de Moeda")) {
+                String input = JOptionPane.showInputDialog("Insira um valor: ");
+                if (funcao.checar(input)) {
+                    double valorRecebido = Double.parseDouble(input);
+                    funcao.converterMoeda(valorRecebido);
+
+                    int resposta;
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                    if ((resposta == 0) && (funcao.checar(input))) {
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        break;
+                    }
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Valor inválido");
+                }
+            } else if (opcao.equals("Conversor de Temperatura")) {
+                String input = JOptionPane.showInputDialog("Insira um valor de temperatura para converter");
+                if (funcao.checar(input)) {
+                    double valorRecebido = Double.parseDouble(input);
+                    funcao.converterTemperatura(valorRecebido);
+
+                    int resposta;
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                    if ((resposta == 0) && (funcao.checar(input))) {
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        break;
+                    }
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Valor inválido");
+                }
             }
-
-        } else {
-            JOptionPane.showMessageDialog(null, "Valor inválido");                
         }
-    } else if (opcao == "Conversor de Temperatura") {
-    	 String input = JOptionPane.showInputDialog("Insira um valor de temperatura para converter");
-         if(f.check(input) == true) {
-             double Minput = Double.parseDouble(input);
-             f.ConverterTemperatura(Minput);
-
-             int resposta = 0;
-             resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
-             if((resposta == 0) && (f.check(input) == true)) { 
-             } else {
-                 JOptionPane.showMessageDialog(null, "Programa finalizado");
-                 break;
-             }
-
-         } else {
-             JOptionPane.showMessageDialog(null, "Valor inválido");                
-         }
-    	}
     }
- }
-
 }
